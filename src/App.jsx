3ce61d4c11bcce2c095c458f7f860338111d1152
emelysarcenobravo <emelybravo852@gmail.com>
2025-08-24 12:36:38 -0700

    sorting and filtering through movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Star from "./assets/images/Star.png"
+import MovieList from "./components/MovieList/MovieList";
 
 const App = () => {
   return(
     <div className='app'>
-      <main>Main Content</main>
+      <Navbar />
+
+      <main>
+        <MovieList type="popular" title="Popular"/>
+        <MovieList type="top_rated" title="Top Rated"/>
+        <MovieList type="upcoming" title="Upcoming"/>
+
+
+      </main>
     </div>
   );
 };
 
-export default App
+export default App;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+    return(
+    <nav className = "navbar">
+        <h1>Movie Selector</h1>
+        <div className="navbar_links">
+            <a href="#popular">Popular</a>
+            <a href="#top_rated">Top Rated</a>
+            <a href="#upcoming">Upcoming Releases</a>
+        </div>
+    </nav>
+    );
+};
+
+export default Navbar;
